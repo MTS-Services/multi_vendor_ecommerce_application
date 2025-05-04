abb3e18b0c78e391d787c4ffe5f48775857f7dba
Md Shariful Islam <devshariful99@gmail.com>
2025-05-04 15:37:25 +0600

    Category model and migration done

---
+++ app/Models/Category.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends BaseModel
+{
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->appends = array_merge(parent::getAppends(), [
+            'status_labels',
+            'status_label',
+            'status_colors',
+            'status_color',
+
+            'status_btn_labels',
+            'status_btn_label',
+            'status_btn_colors',
+            'status_btn_color',
+
+            'featured_labels',
+            'featured_label',
+            'featured_colors',
+            'featured_color',
+
+            'featured_btn_labels',
+            'featured_btn_label',
+            'featured_btn_colors',
+            'featured_btn_color',
+        ]);
+    }
+    public const STATUS_ACTIVE = 1;
+    public const STATUS_DEACTIVE = 0;
+    // Status labels
+    public static function getStatusLabels(): array
+    {
+        return [
+            self::STATUS_ACTIVE => 'Active',
+            self::STATUS_DEACTIVE => 'Deactive',
+        ];
+    }
+
+
+
+    // Status colors
+    public static function getStatusColors(): array
+    {
+        return [
+            self::STATUS_ACTIVE => 'bg-success', // Green for active
+            self::STATUS_DEACTIVE => 'bg-warning', // Red for deactive
+        ];
+    }
+
+    // Status btn labels
+    public static function getStatusBtnLabels(): array
+    {
+        return [
+            self::STATUS_ACTIVE => 'Deactive',
+            self::STATUS_DEACTIVE => 'Active',
+        ];
+    }
+
+    // Status btn colors
+    public static function getStatusBtnColors(): array
+    {
+        return [
+            self::STATUS_ACTIVE => 'btn btn-warning', // Green for active
+            self::STATUS_DEACTIVE => 'btn btn-success', // Red for deactive
+        ];
+    }
+
+    // Accessor for status labels
+    public function getStatusLabelsAttribute(): array
+    {
+        return self::getStatusLabels();
+    }
+
+    // Accessor for status label
+    public function getStatusLabelAttribute(): string
+    {
+        return self::getStatusLabels()[$this->status] ?? 'Unknown';
+    }
+    // Accessor for status color
+    public function getStatusColorAttribute(): string
+    {
+        return self::getStatusColors()[$this->status] ?? 'bg-secondary';
+    }
+
+    // Accessor for status label
+    public function getStatusBtnLabelAttribute(): string
+    {
+        return self::getStatusBtnLabels()[$this->status] ?? 'Unknown';
+    }
+
+    // Accessor for status btn color
+    public function getStatusBtnColorAttribute(): string
+    {
+        return self::getStatusBtnColors()[$this->status] ?? 'btn btn-secondary';
+    }
+
+
+    public const NOT_FEATURED = 0;
+    public const FEATURED = 1;
+
+     // Feature labels
+     public static function getFeaturedLabels(): array
+     {
+         return [
+             self::NOT_FEATURED => 'No',
+             self::FEATURED => 'Yes',
+         ];
+     }
+
+
+
+     // Featured colors
+     public static function getFeaturedColors(): array
+     {
+         return [
+             self::NOT_FEATURED => 'bg-warning',
+             self::FEATURED => 'bg-info',
+         ];
+     }
+
+     // Featured btn labels
+     public static function getFeaturedBtnLabels(): array
+     {
+         return [
+             self::NOT_FEATURED => 'Make Featured',
+             self::FEATURED => 'Remove From Featured',
+         ];
+     }
+
+     // Featured btn colors
+     public static function getFeaturedBtnColors(): array
+     {
+         return [
+             self::NOT_FEATURED => 'btn btn-info',
+             self::FEATURED => 'btn btn-warning',
+         ];
+     }
+
+     // Accessor for featured labels
+     public function getFeaturedLabelsAttribute(): array
+     {
+         return self::getFeaturedLabels();
+     }
+
+     // Accessor for featured label
+     public function getFeaturedLabelAttribute(): string
+     {
+         return self::getFeaturedLabels()[$this->is_featured] ?? 'Unknown';
+     }
+     // Accessor for featured color
+     public function getFeaturedColorAttribute(): string
+     {
+         return self::getFeaturedColors()[$this->is_featured] ?? 'bg-secondary';
+     }
+
+     // Accessor for featured label
+     public function getFeaturedBtnLabelAttribute(): string
+     {
+         return self::getFeaturedBtnLabels()[$this->is_featured] ?? 'Unknown';
+     }
+
+     // Accessor for featured btn color
+     public function getFeaturedBtnColorAttribute(): string
+     {
+         return self::getFeaturedBtnColors()[$this->is_featured] ?? 'btn btn-secondary';
+     }
+
+
+     public function category(): BelongsTo
+     {
+         return $this->belongsTo(Category::class, 'parent_id');
+     }
+
+     public function sub_categories(): HasMany
+     {
+         return $this->hasMany(Category::class, 'parent_id', 'id');
+     }
+
+
+
+
+}
