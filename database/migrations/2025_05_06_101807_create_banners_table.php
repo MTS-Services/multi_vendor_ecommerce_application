bdb7388e8b49679b92914f1f3e3513cb01c434f4
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-05-06 16:43:22 +0600

    model,migration,request setup done

---
+++ database/migrations/2025_05_06_101807_create_banners_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Http\Traits\AuditColumnsTrait;
+
+return new class extends Migration
+{
+    use SoftDeletes, AuditColumnsTrait;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('sort_order')->default(0);
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('image');
+            $table->string('url');
+            $table->boolean('status')->default(true);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+            $table->softDeletes();
+            $this->addAdminAuditColumns($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('banners');
+    }
+};
