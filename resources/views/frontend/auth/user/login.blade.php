1f43b20656b6b0be8e2387c4e505e6bffdf330da
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-05-07 15:21:38 +0600

    design done

--- resources/views/frontend/auth/user/login.blade.php
+++ resources/views/frontend/auth/user/login.blade.php
@@ -9,7 +9,7 @@
                     <h2 class="text-3xl font-semibold text-center mb-6">{{ __('Login your account') }}</h2>
                     <form class="space-y-5" action="{{ route('login') }}" method="POST">
                         @csrf
-                        <div class="flex items-center justify-between gap-3">
+                        <div>
                             <label class="input">
                                 <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                     <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
@@ -18,24 +18,29 @@
                                         <circle cx="12" cy="7" r="4"></circle>
                                     </g>
                                 </svg>
-                                <input type="text" placeholder="Username or email"
-                                    name="login" />
-                                <x-frontend.input-error :datas="['errors' => $errors, 'field' => 'login']" />
+                                <input type="text" placeholder="Username or email" name="login" />
                             </label>
-
+                            <x-frontend.input-error :datas="['errors' => $errors, 'field' => 'login']" />
                         </div>
-                        <label class="input">
-                            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
-                                    stroke="currentColor">
-                                    <path
-                                        d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z">
-                                    </path>
-                                    <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
-                                </g>
-                            </svg>
-                            <input type="password" placeholder="Password" name="password" />
-                        </label>
+                        <div>
+                            <label class="input relative">
+                                <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
+                                        stroke="currentColor">
+                                        <path
+                                            d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z">
+                                        </path>
+                                        <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                                    </g>
+                                </svg>
+                                <input type="password" placeholder="Password" name="password" />
+                                <button type="button"
+                                    class="showpassword absolute top-1/2 right-1 transform -translate-y-1/2 w-8 h-8 flex items-center justify-center rounded-full text-text-white bg-bg-accent bg-opacity-70 hover:bg-opacity-100 hover:text-text-white transition-all duration-300 ease-linear">
+                                    <i data-lucide="eye-off" class="w-4 h-4"></i>
+                                </button>
+                            </label>
+                            <x-frontend.input-error :datas="['errors' => $errors, 'field' => 'password']" />
+                        </div>
 
                         <div class="mt-5 flex justify-between items-center gap-5 flex-wrap">
                             <button type="submit" class="btn-primary">{{ __('Login') }}</button>
@@ -72,3 +77,6 @@
         </div>
     </section>
 @endsection
+@push('js')
+    <script src="{{ asset('frontend/js/login.js') }}"></script>
+@endpush
--- resources/views/frontend/auth/user/register.blade.php
+++ resources/views/frontend/auth/user/register.blade.php
@@ -10,52 +10,74 @@
                     <form class="space-y-5" action="{{ route('register') }}" method="POST">
                         @csrf
                         <div class="flex items-center justify-between gap-3">
-                            <label class="w-full">
-                                <input type="text" placeholder="First Name" name="first_name" class="input" />
+                            <div class="w-full">
+                                <label class="w-full">
+                                    <input type="text" placeholder="First Name" name="first_name" class="input" />
+                                </label>
                                 <x-frontend.input-error :datas="['errors' => $errors, 'field' => 'first_name']" />
-                            </label>
-                            <label class="w-full">
-                                <input type="text" placeholder="Last Name" name="last_name" class="input" />
+                            </div>
+                            <div class="w-full">
+                                <label class="w-full">
+                                    <input type="text" placeholder="Last Name" name="last_name" class="input" />
+                                </label>
                                 <x-frontend.input-error :datas="['errors' => $errors, 'field' => 'last_name']" />
-                            </label>
+                            </div>
                         </div>
 
-                        <label class="input">
-                            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
-                                    stroke="currentColor">
-                                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                                </g>
-                            </svg>
-                            <input type="email" placeholder="Email" name="email" />
-                        </label>
+                        <div class="w-full">
+                            <label class="input">
+                                <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
+                                        stroke="currentColor">
+                                        <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                                        <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                                    </g>
+                                </svg>
+                                <input type="email" placeholder="Email" name="email" />
+                            </label>
+                            <x-frontend.input-error :datas="['errors' => $errors, 'field' => 'email']" />
+                        </div>
 
                         <div class="flex items-center justify-between gap-3">
-                            <label class="input">
-                                <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                    <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
-                                        stroke="currentColor">
-                                        <path
-                                            d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z">
-                                        </path>
-                                        <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
-                                    </g>
-                                </svg>
-                                <input type="password" placeholder="Password" name="password" />
-                            </label>
-                            <label class="input">
-                                <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                    <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
-                                        stroke="currentColor">
-                                        <path
-                                            d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z">
-                                        </path>
-                                        <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
-                                    </g>
-                                </svg>
-                                <input type="password" placeholder="Confirm Password" name="password_confirmation" />
-                            </label>
+                            <div class="w-full">
+                                <label class="input relative">
+                                    <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
+                                            stroke="currentColor">
+                                            <path
+                                                d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z">
+                                            </path>
+                                            <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                                        </g>
+                                    </svg>
+                                    <input type="password" placeholder="Password" name="password" />
+                                    <button type="button"
+                                        class="showpassword absolute top-1/2 right-1 transform -translate-y-1/2 w-8 h-8 flex items-center justify-center rounded-full text-text-white bg-bg-accent bg-opacity-70 hover:bg-opacity-100 hover:text-text-white transition-all duration-300 ease-linear">
+                                        <i data-lucide="eye-off" class="w-4 h-4"></i>
+                                    </button>
+                                </label>
+                                <x-frontend.input-error :datas="['errors' => $errors, 'field' => 'password']" />
+
+                            </div>
+                            <div class="w-full">
+                                <label class="input relative">
+                                    <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
+                                            stroke="currentColor">
+                                            <path
+                                                d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z">
+                                            </path>
+                                            <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                                        </g>
+                                    </svg>
+                                    <input type="password" placeholder="Confirm Password" name="password_confirmation" />
+                                    <button type="button"
+                                        class="showpassword absolute top-1/2 right-1 transform -translate-y-1/2 w-8 h-8 flex items-center justify-center rounded-full text-text-white bg-bg-accent bg-opacity-70 hover:bg-opacity-100 hover:text-text-white transition-all duration-300 ease-linear">
+                                        <i data-lucide="eye-off" class="w-4 h-4"></i>
+                                    </button>
+                                </label>
+                                <x-frontend.input-error :datas="['errors' => $errors, 'field' => 'password_confirmation']" />
+                            </div>
                         </div>
 
                         <div class="mt-5 flex justify-between items-center gap-5 flex-wrap">
@@ -78,7 +100,7 @@
                             </button>
                         </div>
 
-                        <div class="text-center text-sm mt-2">
+                        <div class="text-center text-sm mt-4">
                             {{ __('Become a') }} <a href="#"
                                 class="text-text-accent font-medium">{{ __('Seller') }}</a>
                         </div>
@@ -94,3 +116,6 @@
         </div>
     </section>
 @endsection
+@push('js')
+    <script src="{{ asset('frontend/js/login.js') }}"></script>
+@endpush
