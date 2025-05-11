bda7d683d6c2b86321b034fd3e1e0452d67cf63b
Md Shariful Islam <devshariful99@gmail.com>
2025-05-11 15:53:57 +0600

    Cateagory Dynamic --Done

--- app/Http/Controllers/Frontend/HomePageController.php
+++ app/Http/Controllers/Frontend/HomePageController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomePageController extends Controller
 {
     public function home()
     {
-        return view('frontend.pages.home');
+        $data['categories'] = Category::isMainCategory()->active()->featured()->orderBy('name', 'asc')->get();
+        return view('frontend.pages.home', $data);
     }
 }
